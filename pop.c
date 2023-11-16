f84f57382fb8da2b96c951ddbc862fe4d3e9fd7b
yane12 <yane12girma@gmail.com>
2023-11-16 03:12:21 -0800

    pop.c

--- pop.c
+++ pop.c
@@ -1,24 +1,28 @@
 #include "monty.h"
 /**
- *_pop - removes the top element of the stack
- *@stack: stack where top will be removed
- *@line: kine number of the command
- *Return: void
+ *pop - removes the last inserted element
+ *@line_number: number of lines  in the file
+ *@stack: pointer to the pointer to the head
+ *Return: nothing
  */
-void _pop(stack_t **stack, unsigned int line)
+void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *top;
-
-	top = *stack;
-	if (!stack || !*stack)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line);
-		exit(EXIT_FAILURE);
-	}
-
-	*stack = top->next;
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
-
-	free(top);
+stack_t *to_the_end;
+if (stack == NULL)
+{
+printf("L%u: can't pop an empty stack\n", line_number);
+exit(EXIT_FAILURE);
+}
+else if (*stack == NULL)
+{
+printf("L%u: can't pop an empty stack\n", line_number);
+exit(EXIT_FAILURE);
+}
+else
+{
+to_the_end = *stack;
+*stack = (*stack)->next;
+free(to_the_end);
+}
+return;
 }
